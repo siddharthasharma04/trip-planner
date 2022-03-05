731100384247fd20e9719220e35ff05552b1718f
Siddhartha Sharma <siddharthasharma04@outlook.com>
2022-03-05 15:50:56 +0530

    Add files via upload

---
+++ _next/static/DwdCyhQ6g-bKvqQQ3yoZD/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/15-2b512e616a1a1758.js","static/chunks/pages/index-85972349bbcfdf1d.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
